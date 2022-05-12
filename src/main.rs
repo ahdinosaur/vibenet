cb59afb1dfce893410369a9182cccd07e90048d9
Michael Williams <michael.williams@enspiral.com>
2022-05-12 20:49:09 +1200

    woo hoo! 🎉

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use artnet_protocol::{ArtCommand, Error as ArtError, Output, Poll, PortAddress};
+use palette::{convert::IntoColor, Hsl, Srgb};
 use std::io::Error as IoError;
 use std::net::{ToSocketAddrs, UdpSocket};
 use std::thread::sleep;
@@ -29,14 +30,25 @@ pub enum ArtServerError {
 
 impl ArtServer {
     pub fn new() -> Self {
-        let output_fn = Box::new(|time: f32| {
-            vec![
-                ((time * 0.1_f32).sin() * 256_f32) as u8,
-                ((time * 0.2_f32).sin() * 256_f32) as u8,
-                ((time * 0.01_f32).sin() * 256_f32) as u8,
-                0,
-            ]
-        });
+        let hue_mult = 11_f32;
+        let white_mult = 0.7_f32;
+        let output_fn = move |time: f32| {
+            let hue = time * hue_mult;
+            let color = Hsl::new(hue, 1_f32, 0.5_f32);
+            let rgb_f: Srgb = color.into_color();
+            let rgb_u: Srgb<u8> = rgb_f.into_format();
+            let (r, g, b) = rgb_u.into_components();
+
+            let white = ((((time / white_mult).sin() + 1_f32) / 2_f32) * 64_f32) as u8;
+
+            vec![r, g, b, white]
+            // vec![0, 0, 0, white]
+        };
+
+        let time_offset = |fun: Box<dyn Fn(f32) -> Vec<u8>>, offset: f32| {
+            Box::new(move |time: f32| fun(time + offset))
+        };
+        let offset_mult = 20_f32 / hue_mult;
 
         Self {
             start_time: Instant::now(),
@@ -44,51 +56,59 @@ impl ArtServer {
             fixtures: vec![
                 Fixture::from(RGBW {
                     address: 0,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 0_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 4,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 1_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 8,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 2_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 12,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 3_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 16,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 4_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 20,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 5_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 24,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 6_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 28,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 7_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 32,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 8_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 36,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 9_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 40,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 10_f32 * offset_mult),
                 }),
                 Fixture::from(RGBW {
                     address: 44,
-                    output_fn: output_fn.clone(),
+                    output_fn: time_offset(Box::new(output_fn), 11_f32 * offset_mult),
+                }),
+                Fixture::from(RGBW {
+                    address: 57,
+                    output_fn: time_offset(Box::new(output_fn), 11_f32 * offset_mult),
+                }),
+                Fixture::from(RGBW {
+                    address: 61,
+                    output_fn: time_offset(Box::new(output_fn), 11_f32 * offset_mult),
                 }),
             ],
         }
@@ -156,7 +176,7 @@ impl ArtServer {
         let socket = self.socket.as_ref().unwrap();
 
         loop {
-            let mut data = vec![0; 48];
+            let mut data = vec![0; 65];
             let time = self.start_time.elapsed().as_secs_f32();
 
             for fixture in self.fixtures.iter_mut() {
@@ -180,7 +200,7 @@ impl ArtServer {
                 .send_to(&bytes, &addr)
                 .map_err(|err| ArtServerError::Io(err))?;
 
-            sleep(Duration::from_millis(30));
+            sleep(Duration::from_millis(5));
         }
     }
 }
