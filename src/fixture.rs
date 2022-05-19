2f036409bee645dc1e6d10cb6a586b4aead38e8b
Michael Williams <michael.williams@enspiral.com>
2022-05-19 20:39:54 +1200

    more general fixture trait
    to support lines of RGBWs

--- src/fixture.rs
+++ src/fixture.rs
@@ -2,16 +2,14 @@ use enum_dispatch::enum_dispatch;
 
 #[enum_dispatch]
 pub trait FixtureControl {
-    fn index(&self) -> usize;
-    fn length(&self) -> usize;
-    fn output(&mut self, time: f32) -> Vec<u8>;
+    fn channels(&self) -> Box<dyn Iterator<Item = usize>>;
+    fn output(&mut self, time: f32) -> Box<dyn Iterator<Item = u8>>;
 
     fn write_output(&mut self, output: &mut Vec<u8>, time: f32) {
-        let mut index = self.index();
-        let length = self.length();
-        for input in self.output(time).into_iter().take(length) {
-            output[index] = input;
-            index += 1;
+        let channels = self.channels();
+        let outputs = self.output(time);
+        for (channel, input) in channels.zip(outputs) {
+            output[channel] = input
         }
     }
 }
--- src/fixtures/rgbw.rs
+++ src/fixtures/rgbw.rs
@@ -8,18 +8,14 @@ pub struct RGBW {
 }
 
 impl FixtureControl for RGBW {
-    fn index(&self) -> usize {
-        self.index
+    fn channels(&self) -> Box<dyn Iterator<Item = usize>> {
+        Box::new(self.index..(self.index + 4))
     }
 
-    fn length(&self) -> usize {
-        4
-    }
-
-    fn output(&mut self, time: f32) -> Vec<u8> {
+    fn output(&mut self, time: f32) -> Box<dyn Iterator<Item = u8>> {
         let (r, g, b) = (self.rgb_fun)(time);
         let w = (self.white_fun)(time);
 
-        vec![r, g, b, w]
+        Box::new(vec![r, g, b, w].into_iter())
     }
 }
